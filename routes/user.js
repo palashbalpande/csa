c1953064c41b4281ca10f04a357f97d00e4d116e
Palash Balpande <palashbalpande37@gmail.com>
2025-03-13 09:57:15 +0000

    defined the base structure & routes

---
+++ routes/user.js
@@ -0,0 +1,16 @@
+// const express = require("express");
+// const router = express.Router();
+
+const { Router } = require("express");
+
+const userRouter = Router();
+
+userRouter.post("/signup", function (req, res) {});
+
+userRouter.post("/signin", function (req, res) {});
+
+userRouter.get("/purchases", function (req, res) {});
+
+module.exports = {
+  userRouter,
+};
